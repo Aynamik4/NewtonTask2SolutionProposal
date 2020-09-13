fb79d3f0faaf1c6f009aab4443c409fb1253c2ed
Aynamik4 <violintailgut@gmail.com>
2020-09-13 16:53:54 +0200

    Created The Project

---
+++ NewtonTask2SolutionProposalProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NewtonTask2SolutionProposalProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
